77065891f856bfff4be35b00f8b23264846aeb6d
pythonking28 <nirajthakur2828@gmail.com>
2024-07-09 14:16:13 +0545

    Routes added for channel profile and watch history

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -9,6 +9,8 @@ import {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -35,12 +37,14 @@ router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentUserPassword);
 router.route("/get-user").post(verifyJWT, getCurrentUser);
-router.route("/update-account-details").post(verifyJWT, updateAccountDetails);
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
 router
   .route("/update-user-avatar")
-  .post(verifyJWT, upload.single("avatar"), updateUserAvatar);
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
   .route("/update-user-cover-image")
-  .post(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/user-channel-profile/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 
 export default router;
